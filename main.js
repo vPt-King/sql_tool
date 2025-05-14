338aaf10e153da689c89e3ae18225187a2edc90d
Vũ Phương Thanh <thanh.vp@dsc.com.vn>
2025-05-14 10:45:59 +0700

    add ejs

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
-const express = require('express')
-const morgan = require('morgan')
-const app = express()
-const port = 3000
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const port = 3000;
 
-app.use(morgan('combined'))
+app.use(morgan('combined'));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World haha')
-})
+  res.render("index");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
